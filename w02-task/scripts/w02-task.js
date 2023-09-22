4117bc1ac92cef88e259537575c319a2c9cdd900
terdeck <taeraesuu@gmail.com>
2023-09-22 13:25:19 -0400

    update w02

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -4,19 +4,27 @@
 
 /* Step 2 - Variables */
 
+let fullName = "Tara Decker";
+let currentYear = "2023";
+let profilePicture = "images/td_headshot.jpg";
 
 
 
 /* Step 3 - Element Variables */
 
-
+const nameElement = document.getElementById("name");
+const foodElement = document.getElementById("food");
+const yearElement = document.querySelector("#year");
+const imageElement = document.getElementsByName("image")
 
 
 
 /* Step 4 - Adding Content */
 
-
-
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+let text = document.getElementById("currentYear").textContent;
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `Profile image of ${nameElement}`);
 
 
 
