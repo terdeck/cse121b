2f537088e2b8c473956ac91dce0e85dfcf58ef87
terdeck <taeraesuu@gmail.com>
2023-09-23 22:29:04 -0400

    update w02 hw

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -8,48 +8,36 @@ let fullName = "Tara Decker";
 let currentYear = "";
 let profilePicture = "images/td_headshot.jpg";
 
-
-
 /* Step 3 - Element Variables */
-
 const nameElement = document.getElementById("name");
 const foodElement = document.getElementById("food");
 const yearElement = document.querySelector("#year");
 
-// const imageElement = document.getElementsByName(profilePicture);
-const imageElement = document.getElementById("img");
-
-
+const imageElement = document.querySelector("img");
 
 /* Step 4 - Adding Content */
-
 nameElement.innerHTML = `<strong>${fullName}</strong>`;
-// let text = document.getElementById("currentYear").textContent;
 currentYear.textContent = "2023";
 
 imageElement.setAttribute('src', profilePicture);
 imageElement.setAttribute('alt', `Profile image of ${nameElement}`);
 
-
 /* Step 5 - Array */
-
-let favoriteFood = ['Shrimp Boil', 'Chicken', 'Pepperoni Pizza'];
-
+// 1  
+const favoriteFood = ["Shrimp Boil", " Chicken", " Pepperoni Pizza"];
+// 2
 document.getElementById("food").innerHTML = favoriteFood;
-
-const newFood = favoriteFood + newFood;
-
-favoriteFood.push("Carbs");
-
+// 3
+const newFood = " Carbs";
+// 4
+favoriteFood.push(newFood);
+// 5
 foodElement.innerHTML += `<br>${favoriteFood}`;
-
+// 6
 favoriteFood.shift();
-
-// favoriteFood.log(favoriteFood).innerHTML += `<br>${favoriteFood}`;
-favoriteFood.innerHTML += `<br>${favoriteFood}`;
-
+// 7
+foodElement.innerHTML += `<br>${favoriteFood}`;
+// 8
 favoriteFood.pop();
-
-favoriteFood.innerHTML += `<br>${favoriteFood}`;
-
-
+// 9
+foodElement.innerHTML += `<br>${favoriteFood}`;
