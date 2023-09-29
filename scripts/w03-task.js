b9d1bc937ef86e707ffb6c62b66f736ad35e46a9
terdeck <taeraesuu@gmail.com>
2023-09-28 21:52:40 -0400

    update w3 hmk

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -31,15 +31,16 @@ document.querySelector('#subtractNumbers').addEventListener("click", subtractNum
 // }
 const multiply = (factor1, factor2) => factor1 * factor2;
 // const func = (x) => x * x;
+// ({ a, b } = { a: 10, b: 20 }) => expression
 
 // function multiplyNumbers() {
 //     let multiply1 = Number(document.querySelector('#factor1').value);
 //     let multiply2 = Number(document.querySelector('#factor2').value);
-//     document.querySelector('#product').value = multipy(factor1, factor2);
+//     document.querySelector('#product').value = multipy(multiply1, multiply2);
 // }
 // document.querySelector('#multiplyNumbers').addEventListener("click", multiplyNumbers);
 
-const multiplyNumbers = () => document.querySelector('#product').value = multiply(factor1, factor2) = Number(document.querySelector('#factor1').value) * Number(document.querySelector('#factor2').value);
+const multiplyNumbers = () => document.querySelector('#product').value = multiply(Number(document.querySelector('#factor1').value), Number(document.querySelector('#factor2').value));
 document.querySelector('#multiplyNumbers').addEventListener("click", multiplyNumbers);
 
 /* Open Function Use - Divide Numbers */
@@ -55,17 +56,27 @@ function divideNumbers() {
 document.querySelector('#divideNumbers').addEventListener("click", divideNumbers);
 
 /* Decision Structure */
-
+let date = new Date();
+let currentYear = date.getFullYear();
+document.querySelector('#year').value = currentYear;
 
 /* ARRAY METHODS - Functional Programming */
 /* Output Source Array */
+let new_array = [1,2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+document.querySelector('#array').innerHTML = new_array;
 
 /* Output Odds Only Array */
+document.querySelector('#odds').innerHTML = new_array.filter((odds) => odds % 2 === 1);
 
 /* Output Evens Only Array */
+document.querySelector('#evens').innerHTML = new_array.filter((evens) => evens % 2 === 0);
 
 /* Output Sum of Org. Array */
+document.querySelector('#sumOfArray').innerHTML = new_array.reduce((sum, number) => sum + number);
+// const gpa = gpaPoints.reduce((total, item) => total + item) / gpaPoints.length;
 
 /* Output Multiplied by 2 Array */
+document.querySelector('#multiplied').innerHTML = new_array.map((number) => number * 2);
 
 /* Output Sum of Multiplied by 2 Array */
+document.querySelector('#sumOfMultiplied').innerHTML = new_array.map((number) => number * 2) + new_array.reduce((sum, number) => sum + number);
