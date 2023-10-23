8b21188027796451adef39fc8097fbe04d3a72e1
terdeck <taeraesuu@gmail.com>
2023-10-23 16:49:53 -0400

    update w7 hmk

--- scripts/w06-final-project.js
+++ scripts/w06-final-project.js
@@ -1,25 +1,28 @@
 const url = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
-let results = null;
+// let results = null;
 
-const texts = document.querySelectorAll(".text");
-const body = document.getElementById("body");
-const button = document.querySelectorAll(".new-quote");
+const quotesElement = document.getElementById("text");
+const chooseQuote = document.getElementById("chooseQuote");
+// const button = document.querySelectorAll(".new-quote");
+const button = document.querySelectorAll(".button");
 
-const displayQuote = (quotes) =>{
-    quotesElement.innerHTML = "";
-    quotes.forEach((quote) => {
-        const quoteArticle = document.createElement("article");
-        const quoteTopic = document.createElement("h3");
-        quoteTopic.textContent = quote.quoteTopic;
-        quoteArticle.appendChild(quoteTopic);
-        quoteElement.appendChild(quoteArticle);
-    });
+const displayQuote = (quote) =>{
+    quotesElement.textContent = quote;
+    // quotesElement.innerHTML = "";
+    // quotes.forEach((quote) => {
+    //     const quoteArticle = document.createElement("article");
+    //     const quoteTopic = document.createElement("h3");
+    //     quoteTopic.textContent = quote.quoteTopic;
+    //     quoteArticle.appendChild(quoteTopic);
+    //     quoteElement.appendChild(quoteArticle);
 }
 async function getQuotes(url) {
-    const response = await fetch(url);
+    const topic = chooseQuote.value;
+    const response = await fetch(`${url}${topic}`);
     if (response.ok) {
-        await response.json();
-        displayQuote;
+        const quoteArray = await response.json();
+        const quote = quoteArray[0];
+        displayQuote(quote);
     }
 }
 function reset() {
@@ -31,10 +34,12 @@ function sortBy(quotes) {
     switch (filter) {
         // breakfast government random
         case "breakfast":
-            displayQuotes(quotes.filter((quote) => quote.topic.toLowerCase().includes("breakfast")));
+            // displayQuotes(quotes.filter((quote) => quote.topic.toLowerCase().includes("breakfast")));
+            displayQuotes(quotes.filter((quote) => quote.topic.includes("breakfast")));
             break;
         case "government":
-            displayQuotes(quotes.filter((quote) => quote.topic.toLowerCase().includes("government")));
+            // displayQuotes(quotes.filter((quote) => quote.topic.toLowerCase().includes("government")));
+            displayQuotes(quotes.filter((quote) => quote.topic.includes("government")));
             break;
         case "random":
             displayQuotes(quotes);
@@ -42,4 +47,8 @@ function sortBy(quotes) {
     }
 }
 
-document.querySelector("#sortBy").addEventListener("change", () => { sortBy(results)});
+// document.querySelector("#sortBy").addEventListener("change", () => { sortBy(results)});
+button.addEventListener("click", getQuotes)
+chooseQuote.addEventListener("change", getQuotes);
+
+getQuotes();
