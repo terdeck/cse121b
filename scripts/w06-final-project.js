447f6ef09b399319522480a4979d9a3815f6fb85
terdeck <taeraesuu@gmail.com>
2023-10-23 15:44:28 -0400

    update w7 hmk

--- scripts/w06-final-project.js
+++ scripts/w06-final-project.js
@@ -1,17 +1,45 @@
-{/* <select id="chooseQuote" title="Choose a quote topic:">
-<option value="" disabled selected>Quote Topics</option>
-<option value="breakfast">Breakfast</option>
-<option value="government">Government</option>
-<option value="random">Random</option> */}
-
 const url = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
-// const quoteElement = document.getElementById("quotes");
 let results = null;
 
+const texts = document.querySelectorAll(".text");
+const body = document.getElementById("body");
+const button = document.querySelectorAll(".new-quote");
+
+const displayQuote = (quotes) =>{
+    quotesElement.innerHTML = "";
+    quotes.forEach((quote) => {
+        const quoteArticle = document.createElement("article");
+        const quoteTopic = document.createElement("h3");
+        quoteTopic.textContent = quote.quoteTopic;
+        quoteArticle.appendChild(quoteTopic);
+        quoteElement.appendChild(quoteArticle);
+    });
+}
 async function getQuotes(url) {
     const response = await fetch(url);
     if (response.ok) {
-        const data = await response.json();
-        doStuff(data);
+        await response.json();
+        displayQuote;
     }
 }
+function reset() {
+    quotesElement.innerHTML = "";
+}
+function sortBy(quotes) {
+    reset();
+    const filter = document.getElementById("sortBy").value;
+    switch (filter) {
+        // breakfast government random
+        case "breakfast":
+            displayQuotes(quotes.filter((quote) => quote.topic.toLowerCase().includes("breakfast")));
+            break;
+        case "government":
+            displayQuotes(quotes.filter((quote) => quote.topic.toLowerCase().includes("government")));
+            break;
+        case "random":
+            displayQuotes(quotes);
+            break;
+    }
+}
+
+document.querySelector("#sortBy").addEventListener("change", () => { sortBy(results)});
